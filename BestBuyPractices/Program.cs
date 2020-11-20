23667a5b68d00d9bfaf034e3a926ff35c4d4c0bf
Olakunle E Neye <emmanuelneye@gmail.com>
2020-11-20 11:28:49 -0500

    Configure appsettings.json and created MySQL connection from SQL DB to application

--- BestBuyPractices/Program.cs
+++ BestBuyPractices/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace BestBuyPractices
 {
@@ -6,7 +10,17 @@ namespace BestBuyPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connection = GetConnection("appsettings.json", "DefaultConnection");
+        }
+        public static IDbConnection GetConnection(string appJSONConfig, string connectionName)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(appJSONConfig)
+                .Build();
+            string connectionString = config.GetConnectionString(connectionName);
+            IDbConnection connection = new MySqlConnection(connectionString);
+            return connection;
         }
     }
 }
